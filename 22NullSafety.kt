5b2545528a5a1b42dc8fa5e7d5d59af521285202
Binayak Niraula <binayak0128@gmail.com>
2023-11-25 09:51:40 +0545

    Files Added

---
+++ 22NullSafety.kt
@@ -0,0 +1,25 @@
+fun main()
+{
+    var str:String = "Hello"
+    println(str)
+
+//    str = null //Variable can not hold null by default
+//    println(str)
+
+    var num:Int = 234
+    println(num)
+
+//    num=null //Variable can not hold null by default
+//    println(num)
+
+    //To make variable hold null we can use ?
+    var str2:String? = "Binayak"
+    str2 = null
+    println(str2)
+
+    var num2:Int?=2345
+    num2=null
+    println(num2)
+
+
+}
